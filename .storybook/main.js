6d64306eb118918d0c1092606d435d705800f3af
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-09-01 15:30:42 -0500

    Updates

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest",
+    "storybook-addon-test-codegen",
+  ],
+  framework: {
+    name: "@storybook/web-components-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
